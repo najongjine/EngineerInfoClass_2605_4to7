aa1e33f9af70c127341bec84efb48fc0767ca968
najongjine <najongjin3@hotmail.com>
2026-06-10 17:08:56 +0900

    .

---
+++ Python/python260610.py
@@ -0,0 +1,16 @@
+"""
+파이썬 기본 자료형
+"""
+a = 3
+print(f"a:{type(a)}") # int
+b=1.1
+print(f"b:{type(b)}") # float
+c='c'
+print(f"c:{type(c)}") # string
+d="c"
+print(f"d:{type(d)}") # string
+e=True
+print(f"e:{type(e)}") # bool
+
+
+
