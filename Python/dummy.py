d78d2e695bc7eb74ad3fc4581377e0ee6f2872ac
najongjine <najongjin3@hotmail.com>
2026-07-08 17:49:03 +0900

    .

--- Python/dummy.py
+++ Python/dummy.py
@@ -1,16 +1,21 @@
-#i = input()
-i="HumanDev"
-x = [] 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
  
-"""
-  "HumanDev"   ["HumanDev"]
-"""
-for word in i.split():
-    # x=["HumanDev"]
-    x.append(word)
-
-# y="HumanDev"
-y = ''.join(x)
-z = ''.join(c for c in y[::-1] if c not in 'ong')
+def tree(li):
+    nodes = [Node(i) for i in li]
+    for i in range(1, len(li)):
+        nodes[(i - 1) // 2].children.append(nodes[i])
+    return nodes[0]
  
-print(z)
+def calc(node, level=0):
+    if node is None:
+        return 0
+    return (node.value if level % 2 == 1 else 0) + sum(calc(n, level + 1) for n in node.children)
+ 
+li = [3, 5, 8, 12, 15, 18, 21]
+ 
+root = tree(li)
+ 
+print(calc(root)) 
